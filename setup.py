d255a42689b70f0a98d06e2fb0215f7e2778153c
Boris Yangel <boris.jangel@gmail.com>
2020-05-02 18:16:23 +0300

    Basic package structure.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+      name='trackdays',
+      version='0.1',
+      description='An RL environment and training code for a car on a racetrack.',
+      url='http://github.com/hr0nix/trackdays',
+      author='Boris Yangel',
+      author_email='boris.jangel@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False,
+      python_requires='>=3.6',
+      install_requires=[
+            'tensorflow',
+            'tf-agents',
+      ],
+      dependency_links=[
+            'git+https://github.com/eleurent/highway-env',
+      ],
+)
